c1b271b223a9f6e647b917fe75a32ef9c85218b4
Huy <truongvanhuy2000@gmail.com>
2023-05-26 00:48:38 +0700

    fixed

--- nfsclient/entrypoint.sh
+++ nfsclient/entrypoint.sh
@@ -3,11 +3,7 @@
  
 ## Export NFS Mount
 mkdir /video
-mount -t nfs "$NFS_SERVER_IP:/data/resources" /video
-
-if mount | grep "/video"; then
-  echo "NFS share mounted successfully!"
-else
-  echo "ERROR: Failed to mount the NFS share."
-  exit 1
-fi
+service rpcbind start
+service nfs-common start
+mount -v $NFS_SERVER_IP:/ /video
+ls -a /video

19b929a9f78414f916ae483b496d2b98b308b9a4
huy <truongvanhuy2000@gmail.com>
2023-05-26 16:44:20 +0700

    adss

--- nfsclient/entrypoint.sh
+++ nfsclient/entrypoint.sh
@@ -9,20 +9,6 @@
 # ls -a /video
 
 MOUNT_SUCCESS=false
-service rpcbind start
-service nfs-common start
-mkdir -p /video
-while [ $MOUNT_SUCCESS = false ]; do
-    mount $NFS_SERVER_IP:/ /video
-
-    if [ $? -eq 0 ]; then
-        echo "Mount sucessfully"
-        MOUNT_SUCCESS=true
-    else
-        echo "Mount failed. Retrying in 10 seconds..."
-        sleep 10
-    fi
-done
-
+sleep 5
 ls -a /video
-/bin/bash
+bash

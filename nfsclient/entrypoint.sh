868daca88e2293db71244efbd98ca6dea5641161
huy <truongvanhuy2000@gmail.com>
2023-05-26 09:44:12 +0700

    done

--- nfsclient/entrypoint.sh
+++ nfsclient/entrypoint.sh
@@ -2,8 +2,27 @@
 ## Application-specific logic and mounting NVME storage removed ##
  
 ## Export NFS Mount
-mkdir /video
-service rpcbind start
-service nfs-common start
-mount -v $NFS_SERVER_IP:/ /video
+# mkdir /video
+# service rpcbind start
+# service nfs-common start
+# mount -v $NFS_SERVER_IP:/ /video
+# ls -a /video
+
+MOUNT_SUCCESS=false
+
+while [ $MOUNT_SUCCESS = false ]; do
+    mkdir -p /video
+    service rpcbind start
+    service nfs-common start
+    mount -v $NFS_SERVER_IP:/ /video
+
+    if [ $? -eq 0 ]; then
+        echo "Mount sucessfully"
+        MOUNT_SUCCESS=true
+    else
+        echo "Mount failed. Retrying in 10 seconds..."
+        sleep 10
+    fi
+done
+
 ls -a /video
